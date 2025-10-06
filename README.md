b3ad23f47946a1b0c0459f21fb73cf43f077f0fd
Bkeefe901 <bradyjokeefe@gmail.com>
2025-10-06 11:58:19 -0400

    Basic set up of react, ideas in README

---
+++ README.md
@@ -0,0 +1,15 @@
+# SBA 320H - React Web Application Project
+
+
+// Ideas:
+1. Rick and Morty Universe
+    -Pages:
+        a. Characters
+        b. Locations
+        c. Episodes
+2. Black Jack Card Game
+    -Pages:
+        a. HomePage/Instructions
+        b. Game page
+        c. 404 page
+3
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect, useReducer } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
