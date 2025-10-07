58d9656e7e65ac5158b73e7faada8ea7b2bb392c
Bkeefe901 <bradyjokeefe@gmail.com>
2025-10-07 17:48:52 -0400

    Abstracted cards to Cards component, and created state for dealer cards and player cards

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -3,22 +3,23 @@ import axios from "axios";
 
 
 export default function GamePage() {
-    // array of cards from deck that have been dealt
+    // array of cards for the user to be dealt from original deck
     const [data, setData] = useState([]);
 
-    // Sets the unique code for a deck, from the api, so that on each new card it comes from the same deck
-    const [deck, setDeck] = useState(null);
+    // array of cards for dealer to be dealt from original deck
+    const [dealerData, setDealerData] = useState([]);
 
-    // Sets the number of cards to be drawn from the deck
-    const [dealCards, setDealCards] = useState(3);
+    // Sets the unique code for a deck, from the api, so that on each new card it comes from the same deck
+    //const [deck, setDeck] = useState(null);
+
 
     // Sets who is receiving the new card
     const [receiver, setReceiver] = useState();
-    
+
     //const [userCards, setUserCards] = useReducer(cardReducer, initialState);
 
 
-    let urlStr = `https://deckofcardsapi.com/api/deck/${deck ? deck : "new"}/draw/?count=${dealCards}`;
+    let urlStr = `https://deckofcardsapi.com/api/deck/new/draw/?count=24`;
     // need conditional str like:
     // // let urlStr = "https://deckofcardsapi.com/api/${deck ? deck_id : "new"}/new/draw/?count=${cards ? cards : 4}";
 
@@ -29,12 +30,17 @@ export default function GamePage() {
 
                 let res = await axios.get(urlStr);
 
-                //console.log(res.data);
-                setData(prev => [...prev,...res.data.cards]);
-                // need to get the deck id and save it for drawing new cards
-                setDeck(res.data.deck_id);
-                console.log(res.data.deck_id)
-                
+                let cards = [...res.data.cards];
+                let numCards = cards.length;
+
+                let userCards = cards.slice(0, (numCards / 2));
+                let dealerCards = cards.slice((numCards / 2), numCards);
+
+
+                setData(userCards);
+                setDealerData(dealerCards);
+
+
             } catch (err) {
                 console.error(`❌ Error - ${err.message}`);
 
@@ -43,11 +49,11 @@ export default function GamePage() {
 
         getData();
 
-    }, [dealCards]);
+    }, []);
 
 
-  
-    
+
+
     // need to conditionally add new image for each hit 
     // need logic for dealer to draw or hold after player hits stand
     // need to keep track of score bust bust and 
@@ -59,27 +65,19 @@ export default function GamePage() {
 
     let loaded = () => {
 
-        
+
 
         return (
             <>
                 <h2>Dealer's Cards</h2>
-                <h3>Visible Count: {cardValue(data[0].value)}</h3>
-                <div>
-                    <img style={ {height: '315px'}} src="../../public/images/CardBack.png" alt={data[0].code} />
-                    <img src={data[0].image} alt={data[0].code} />
-                </div>
+                <Cards state={dealerData} kind='dealer' />
                 <br />
                 <br />
                 <br />
                 <h2>Your Cards</h2>
-                <h3>Count: {cardValue(data[1].value) + cardValue(data[2].value)}</h3>
-                <div>
-                    <img src={data[1].image} alt={data[1].code} />
-                    <img src={data[2].image} alt={data[2].code} />
-                </div>
+                <Cards state={data} kind='player' />
                 <div className="buttonCluster">
-                    <button onClick={()=>{setDealCards(1)}}>Hit</button>
+                    <button>Hit</button>
                     <button>Stand</button>
                 </div>
             </>
@@ -88,8 +86,44 @@ export default function GamePage() {
 
 
 
-    return data.length > 2 ? loaded() : loading();
+    return data.length > 1 ? loaded() : loading();
 }
+
+
+
+// Components
+
+function Cards({ state, kind }) {
+
+    
+
+    return kind == 'player' ?
+        (
+            <>
+                <h3>Count: {cardValue(state[0].value) + cardValue(state[1].value)}</h3>
+                <div>
+                    <img src={state[0].image} alt={state[0].code} />
+                    <img src={state[1].image} alt={state[1].code} />
+                </div>
+            </>
+        ) :
+        (
+            <>
+                <h3>Visible Count: {cardValue(state[0].value)}</h3>
+                <div>
+                    <img style={{ height: '315px' }} src="../../public/images/CardBack.png" alt={state[0].code} />
+                    <img src={state[0].image} alt={state[0].code} />
+                </div>
+            </>
+        )
+
+
+}
+
+
+
+// Reducer Function
+
 
 
 
@@ -97,18 +131,15 @@ export default function GamePage() {
 // Helper Functions
 
 
-      function cardValue(card){
-        if(card == "ACE"){
-            return 11;
-        }
-        else if(card == 'JACK' || card == 'QUEEN' || card == 'KING'){
-            return 10;
-        }else{
-            return Number(card);
-        }
-        
+function cardValue(card) {
+    if (card == "ACE") {
+        return 11;
+    }
+    else if (card == 'JACK' || card == 'QUEEN' || card == 'KING') {
+        return 10;
+    } else {
+        return Number(card);
     }
 
-    // function softCount(card){
+}
 
-    // }
